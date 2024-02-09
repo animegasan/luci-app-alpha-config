a8753d20d390a84f69cfa871a7e30f33a640a4af
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-02-09 10:25:15 +0700

    Update files
    Replacing the entire structure.

--- htdocs/luci-static/resources/view/alpha-config.js
+++ htdocs/luci-static/resources/view/alpha-config.js
@@ -7,40 +7,47 @@
 'require view';
 'require form';
 'require fs';
+'require ui';
 
 return view.extend({
-    handleSaveApply: null,
-    handleSave: null,
-    handleReset: null,
-    render: function () {
-        var m, s, o;
-        m = new form.Map('alpha', _('Alpha theme configuration'), _('Here you can set the blur and transparency of the login page of alpha theme, and manage the background pictures and videos. Chrome is recommended.'));
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null,
+	render: function () {
+		var m, s, o;
+		var bg_path = '/www/luci-static/alpha/background/';
+		m = new form.Map('alpha' , _('Alpha theme configuration'), _('Here you can set background login and dashboard themes. Chrome is recommended.'));
+		s = m.section(form.TypedSection, null , _('Background configuration'), _('You can upload files such as jpg or png files, and files will be uploaded to <code>%s</code>.').format(bg_path));
+		s.anonymous = true;
 
-        s = m.section(form.TypedSection, 'alpha');
-        s.anonymous = true;
+		o = s.option(form.Button, 'login', _('Login'), _('Upload file for login background'));
+		o.inputstyle = 'action';
+		o.inputtitle = _('Upload');
+		o.onclick = function(ev, section_id) {
+			var file = bg_path + 'login.png';
+			return ui.uploadFile(file, ev.target).then(function(res) {
+				return fs.exec('/bin/chmod', ['0644', file]).then(function() {
+					ui.addNotification(null, E('p', _('Login picture successfully uploaded.')));
+				});
+			})
+			.catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+		};
+		o.modalonly = true;
 
-        o = s.option(form.FileUpload, 'dashboard', _('Dashboard Background'));
-        o.default = '/www/luci-static/alpha/gaya/dashboard.png';
-        o.root_directory = '/www/luci-static/alpha/gaya/';
-        o.optional = true;
+		o = s.option(form.Button, 'dashboard', _('Dashboard'), _('Upload file for dashboard background'));
+		o.inputstyle = 'action';
+		o.inputtitle = _('Upload');
+		o.onclick = function(ev, section_id) {
+			var file = bg_path + 'dashboard.png';
+			return ui.uploadFile(file, ev.target).then(function(res) {
+				return fs.exec('/bin/chmod', ['0644', file]).then(function() {
+					ui.addNotification(null, E('p', _('Dashboard picture successfully uploaded.')));
+				});
+			})
+			.catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+		};
+		o.modalonly = true;
 
-        o = s.option(form.FileUpload, 'login', _('Login Background'));
-        o.default = '/www/luci-static/alpha/style/login.png';
-        o.root_directory = '/www/luci-static/alpha/style/';
-        o.optional = true;
-
-        o = s.option(form.Button, '_save', _('Save settings'));
-        o.inputstyle = 'apply';
-        o.inputtitle = _('Save settings');
-        o.onclick = function() {
-            var gaya = '/www/luci-static/alpha/gaya/dashboard.png';
-            var style = '/www/luci-static/alpha/style/login.png';
-            if (gaya && style) {
-                fs.exec('/bin/chmod', ['644', gaya, style]);
-            }
-            return this.map.save(null, true);
-        }
-
-        return m.render();
-    },
+		return m.render();
+	},
 });
